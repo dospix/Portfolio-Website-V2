672caf3f797e9c3c2d0a01b88528bd14edfa23b1
Daniel <dospinescu_daniel@yahoo.ro>
2023-04-23 16:13:34 +0300

    Added page title

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ function App() {
 
   return (
     <div className="App">
+    <>
+      <div className='text-center'>
+        <h1>Hello, I'm Dospinescu Daniel</h1>
+        <h2>A python programmer who enjoys problem solving</h2>
+      </div>
+    </>
     </div>
   )
 }

2e7d6c451d245f04e623491c41313f51101e815a
Daniel <dospinescu_daniel@yahoo.ro>
2023-04-25 19:08:33 +0300

    Added dark mode slider on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import DarkModeSlider from './components/DarkModeSlider'
 
 function App() {
 
   return (
     <div className="App">
     <>
+      <DarkModeSlider />
       <div className='text-center'>
         <h1>Hello, I'm Dospinescu Daniel</h1>
         <h2>A python programmer who enjoys problem solving</h2>

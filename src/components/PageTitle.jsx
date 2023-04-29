191fe0798e90e4d10ab60ac82a3f0bfbf3a4d459
Daniel <dospinescu_daniel@yahoo.ro>
2023-04-29 13:06:51 +0300

    Fixed mistake in previous commit

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -3,6 +3,6 @@ export default function PageTitle(){
         <div className='mt-10 text-center'>
             <h1 className='text-2xl font-semibold'>Hello, I'm Dospinescu Daniel</h1>
             <h2 className='mt-3 font-extralight' >I am a programmer that uses Python and JavaScript</h2>
-      </div>
+        </div>
     )
 }

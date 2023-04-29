ff2c54da4d07abd0a859823696cb1f7c355b7dec
Daniel <dospinescu_daniel@yahoo.ro>
2023-04-29 13:05:21 +0300

    Created a component for the page title

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import DarkModeSlider from './components/DarkModeSlider'
+import PageTitle from './components/PageTitle'
 
 function App() {
 
@@ -7,10 +8,7 @@ function App() {
     <div className="App">
     <>
       <DarkModeSlider />
-      <div className='mt-10 text-center'>
-        <h1 className='text-2xl font-semibold'>Hello, I'm Dospinescu Daniel</h1>
-        <h2 className='mt-3 font-extralight' >I am a programmer that uses Python and JavaScript</h2>
-      </div>
+      <PageTitle />
     </>
     </div>
   )

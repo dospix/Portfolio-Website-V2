135b565f02b7aa5da9f664d6097710af30c476ba
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-01 17:21:13 +0300

    Added Projects.jsx file for storing projects as objects, and showed them on the main page. Work in progress.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import DarkModeSlider from './components/DarkModeSlider'
 import PageTitle from './components/PageTitle'
+import projects from './Projects'
 
 function App() {
+  const projectsHTML = projects.map(project => <h1 className='mt-10' key={project.key}>{project.name}</h1>)
 
   return (
     <div className="App">
@@ -12,6 +14,9 @@ function App() {
       <div className='mt-20 flex flex-col items-center justify-items-center'>
         <h1 className='text-2xl font-semibold'>Personal Projects</h1>
         <div className='mt-1 h-0.5 w-20 bg-blue-400'></div>
+      </div>
+      <div className='mt-20 text-center'>
+        {projectsHTML}
       </div>
     </>
     </div>

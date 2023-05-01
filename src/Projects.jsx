1ff22b6de3df411956a2445e403f08291fc7322e
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-01 17:21:13 +0300

    Added Projects.jsx file for storing projects as objects, and showed them on the main page. Work in progress.

---
+++ src/Projects.jsx
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        key: 0,
+        name: "a",
+        description: "b",
+        image: "c"
+    },
+    {
+        key: 1,
+        name: "aa",
+        description: "bb",
+        image: "cc"
+    }
+]
+
+export default projects

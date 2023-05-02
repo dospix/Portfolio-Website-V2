2c7a2ae94f4b906556a9fbbd28a5a20d40c3306e
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-02 21:45:30 +0300

    Worked on how projects are displayed on the main page. Work in progress.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,15 +1,21 @@
 const projects = [
     {
         key: 0,
-        name: "a",
-        description: "b",
-        image: "c"
+        name: "Machine learning project",
+        description: "This is a machine learning project using pandas, scikit-learn and PyTorch",
+        image: "./path-to-image"
     },
     {
         key: 1,
-        name: "aa",
-        description: "bb",
-        image: "cc"
+        name: "API project",
+        description: "This is a project that uses an API for functionality",
+        image: "./path-to-image2"
+    },
+    {
+        key: 2,
+        name: "SQL project",
+        description: "This is a frontend project using React and MySQL",
+        image: "./path-to-image3"
     }
 ]
 

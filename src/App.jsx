cdc0d47b30d3ffb7e8ef50c2e96bbd815d235b85
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-11 21:50:24 +0300

    Removed  BORDER_RADIUS_SIZE variable, because Tailwind doesn't condone string interpolation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import DarkModeSlider from './components/DarkModeSlider'
 import PageTitle from './components/PageTitle'
 import projects from './Projects'
 import Footer from './components/Footer'
 
 function App() {
-  const BORDER_RADIUS_SIZE = "xl"
   const projectsHTML = projects.map(project => (
-    <div className={`flex items-center justify-center rounded-${BORDER_RADIUS_SIZE} shadow-md shadow-gray-300 hover:shadow-lg hover:shadow-gray-300 mt-20 mx-96 ${project.key % 2 == 1 ? "flex-row-reverse" : ""}`} key={project.key}>
+    <div className={`flex items-center justify-center rounded-xl shadow-md shadow-gray-300 hover:shadow-lg hover:shadow-gray-300 mt-20 mx-96 ${project.key % 2 == 1 ? "flex-row-reverse" : ""}`} key={project.key}>
       <div className='w-2/3 h-full'>
         <h1>{project.name}</h1>
         <p>{project.description}</p>
@@ -16,7 +14,7 @@ function App() {
         </div>
       </div>
       <div className='w-1/3'>
-        <img src={project.image} className={`h-56 w-full ${project.key % 2 == 1 ? `rounded-s-${BORDER_RADIUS_SIZE}` : `rounded-e-${BORDER_RADIUS_SIZE}`}`} alt='machine learning' />
+        <img src={project.image} className={`h-56 w-full ${project.key % 2 == 1 ? "rounded-s-xl" : "rounded-e-xl"}`} alt='machine learning' />
       </div>
     </div>
   ))

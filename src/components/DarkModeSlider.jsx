cb77cd4c091e1fb96acb1249f0bacd2c99695a67
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-15 21:58:17 +0300

    Changed how the dark mode slider looks, it reacts to the site switching to dark mode and back.

--- src/components/DarkModeSlider.jsx
+++ src/components/DarkModeSlider.jsx
@@ -1,14 +1,16 @@
-import sun from "../assets/images/sun.png"
-import moon from "../assets/images/moon.png"
+import blue_sun from "../assets/images/more-blue-sun.png"
+import gray_sun from "../assets/images/sun-75.png"
+import blue_moon from "../assets/images/more-blue-moon.png"
+import gray_moon from "../assets/images/more-gray-moon.png"
 
 export default function DarkModeSlider(props){
     return (
-        <div className="flex ml-auto mt-6 mr-8 w-32">
-            <img src={sun} height="20px" width="20px" alt="sun" />
-            <div onClick={props.onDarkMode} className="my-auto mx-2 w-12 h-4 bg-gray-400 rounded-lg hover:cursor-pointer">
-                <div className="w-4 h-4 bg-gray-700 rounded-full"></div>
+        <div className="flex ml-auto mt-12 mr-28 w-40">
+            <img src={props.isDarkMode ? gray_sun : blue_sun} height="30px" width="30px" alt="sun" />
+            <div onClick={props.onDarkMode} className="my-auto mx-2 w-16 h-5 bg-gray-400 rounded-3xl hover:cursor-pointer">
+                <div className={`w-5 h-5 bg-gray-700 rounded-full ${props.isDarkMode ? "ml-auto" : ""}`}></div>
             </div>
-            <img src={moon} height="20px" width="20px" alt="moon" />
+            <img src={props.isDarkMode ? blue_moon : gray_moon} height="30px" width="30px" alt="moon" />
         </div>
     )
 }

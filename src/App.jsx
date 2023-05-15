4807af2f2ac691da643b756d1114f73e06c5ff85
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-15 21:58:17 +0300

    Changed how the dark mode slider looks, it reacts to the site switching to dark mode and back.

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <div className='w-2/3 h-full'>
         <h1 className='text-lg mt-4 font-semibold'>{project.name}</h1>
         <p className='mt-8'>{project.description}</p>
-        <div className='mt-10 mx-auto w-28 rounded-xl outline outline-offset-8 outline-1 outline-gray-600 hover:outline-2 hover:outline-blue-400 hover:cursor-pointer'>
+        <div className='mt-10 mx-auto w-28 rounded-xl outline outline-offset-8 outline-1 outline-gray-600 hover:outline-2 hover:outline-blue-500 hover:cursor-pointer'>
           <h2>View Project</h2>
         </div>
       </div>
@@ -29,11 +29,11 @@ function App() {
   return (
     <div className={`App absolute ${darkMode ? "bg-zinc-900 text-white" : ""}`}>
     <>
-      <DarkModeSlider onDarkMode={handleDarkMode}/>
+      <DarkModeSlider onDarkMode={handleDarkMode} isDarkMode={darkMode}/>
       <PageTitle />
       <div className='mt-16 flex flex-col items-center justify-items-center'>
         <h1 className='text-3xl font-semibold'>Personal Projects</h1>
-        <div className='mt-1 h-0.5 w-20 bg-blue-400'></div>
+        <div className='mt-1 h-0.5 w-20 bg-blue-500'></div>
       </div>
       <div className='mt-20 text-center'>
         {projectsHTML}

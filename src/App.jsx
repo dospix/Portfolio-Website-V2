8d290bdc503fceff39d419aa8ca99d420775c548
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-20 12:56:58 +0300

    Added the corrected Open_Sans font throughout the page.

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <div className={`h-56 flex items-center justify-center rounded-xl shadow-[0_2px_10px_0_rgba(0,0,0,0.3)] hover:shadow-[0_2px_15px_0_rgba(0,0,0,0.3)] ${darkMode? "shadow-blue-500 hover:shadow-blue-500" : ""} mt-20 mx-96 ${project.key % 2 == 1 ? "flex-row-reverse" : ""}`} key={project.key}>
       <div className='w-2/3 h-full'>
         <h1 className='mt-6 text-lg font-semibold font-Montserrat'>{project.name}</h1>
-        <p className='mt-8'>{project.description}</p>
+        <p className='mt-8 font-Open_Sans'>{project.description}</p>
         <div className='mt-10 mx-auto w-28 rounded-xl outline outline-offset-8 outline-1 outline-gray-600 hover:outline-2 hover:outline-blue-500 hover:cursor-pointer'>
           <h2 className='text-[0.9rem] font-Montserrat'>View Project</h2>
         </div>

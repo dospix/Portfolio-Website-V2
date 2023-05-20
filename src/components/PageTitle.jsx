0e07a610e3cfd24abec9bd3fc4707c57af269233
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-20 12:54:30 +0300

    Added titles from App to PageTitle component and renamed it PageTitles. Corrected mistake with OpenSans font.

--- src/components/PageTitle.jsx
+++
@@ -1,8 +0,0 @@
-export default function PageTitle(){
-    return (
-        <div className='mt-10 text-center'>
-            <h1 className='text-2xl font-semibold font-Montserrat'>Hello, I'm Dospinescu Daniel</h1>
-            <h2 className='mt-3 font-light'>I am a programmer that uses Python and JavaScript</h2>
-        </div>
-    )
-}

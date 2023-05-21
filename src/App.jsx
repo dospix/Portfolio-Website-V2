83d21be9420a19b666b60fa3e23ae27988dc2007
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-21 16:36:02 +0300

    Made website responsive to smaller devices, work in progress.

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,26 @@ function App() {
   }
 
   const projectsHTML = projects.map(project => (
-    <div className={`h-56 flex items-center justify-center rounded-xl shadow-[0_2px_10px_0_rgba(0,0,0,0.3)] hover:shadow-[0_2px_15px_0_rgba(0,0,0,0.3)] ${darkMode? "shadow-blue-500 hover:shadow-blue-500" : ""} mt-20 mx-96 ${project.key % 2 == 1 ? "flex-row-reverse" : ""}`} key={project.key}>
+    <div className={`2xl:mt-20 lg:mt-10 mx-auto 2xl:h-56 lg:h-44 2xl:w-1/2 lg:w-3/4 flex items-center justify-center rounded-xl shadow-[0_2px_10px_0_rgba(0,0,0,0.3)] hover:shadow-[0_2px_15px_0_rgba(0,0,0,0.3)] ${darkMode? "shadow-blue-500 hover:shadow-blue-500" : ""} ${project.key % 2 == 1 ? "flex-row-reverse" : ""}`} key={project.key}>
       <div className='w-2/3 h-full'>
-        <h1 className='mt-6 text-lg font-semibold font-Montserrat'>{project.name}</h1>
-        <p className='mt-8 font-Open_Sans'>{project.description}</p>
-        <div className='mt-10 mx-auto w-28 rounded-xl outline outline-offset-8 outline-1 outline-gray-600 hover:outline-2 hover:outline-blue-500 hover:cursor-pointer'>
-          <h2 className='text-[0.9rem] font-Montserrat'>View Project</h2>
+        <h1 className='2xl:mt-6 lg:mt-4 h-1/6 text-lg font-semibold font-Montserrat'>{project.name}</h1>
+        <p className='mt-4 h-2/6 font-Open_Sans'>{project.description}</p>
+        <div className='2xl:mt-3 mx-auto w-28 rounded-xl outline outline-offset-8 outline-1 outline-gray-600 hover:outline-2 hover:outline-blue-500 hover:cursor-pointer'>
+          <h2 className='2xl:text-[0.9rem] font-Montserrat'>View Project</h2>
         </div>
       </div>
       <div className='w-1/3 h-full'>
-        <img src={project.image} className={`h-full ${project.key % 2 == 1 ? "rounded-s-xl" : "rounded-e-xl"}`} alt='machine learning' />
+        <img src={project.image} className={`h-full w-full ${project.key % 2 == 1 ? "rounded-s-xl" : "rounded-e-xl"}`} alt='machine learning' />
       </div>
     </div>
   ))
 
   return (
-    <div className={`App absolute ${darkMode ? "bg-zinc-900 text-white" : ""}`}>
+    <div className={`App absolute w-full ${darkMode ? "bg-zinc-900 text-white" : ""}`}>
     <>
       <DarkModeSlider onDarkMode={handleDarkMode} isDarkMode={darkMode}/>
       <PageTitles />
-      <div className='mt-20 text-center'>
+      <div className='2xl:mt-20 lg:mt-10 text-center'>
         {projectsHTML}
       </div>
       <Footer isDarkMode={darkMode} />
--- src/components/DarkModeSlider.jsx
+++ src/components/DarkModeSlider.jsx
@@ -5,7 +5,7 @@ import gray_moon from "../assets/images/gray-moon.png"
 
 export default function DarkModeSlider(props){
     return (
-        <div className="flex ml-auto mt-12 mr-28 w-40">
+        <div className="flex ml-auto 2xl:mt-12 lg:mt-6 2xl:mr-28 lg:mr-20 w-40">
             <img src={props.isDarkMode ? gray_sun : blue_sun} height="30px" width="30px" alt="sun" />
             <div onClick={props.onDarkMode} className="my-auto mx-2 w-16 h-5 bg-gray-400 rounded-3xl hover:cursor-pointer">
                 <div className={`w-5 h-5 bg-gray-700 rounded-full ${props.isDarkMode ? "ml-auto" : ""}`}></div>

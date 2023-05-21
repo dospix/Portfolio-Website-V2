c8207c8c58c4cace667d904751852f5c4c8bd31d
Daniel <dospinescu_daniel@yahoo.ro>
2023-05-21 16:36:02 +0300

    Made website responsive to smaller devices, work in progress.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import linkedin_white from "../assets/images/linkedin-logo-white.svg"
 
 export default function Footer(props){
     return (
-        <footer className={`mt-20 h-28 border-t-2 border-solid ${props.isDarkMode ? "border-slate-700" : ""} flex items-center justify-center space-x-8`}>
+        <footer className={`2xl:mt-20 lg:mt-8 2xl:h-28 lg:h-16 border-t-2 border-solid ${props.isDarkMode ? "border-slate-700" : ""} flex items-center justify-center space-x-8`}>
             <h1>© 2023 Dospinescu Daniel</h1>
             <a href="https://github.com/dospix" target="_blank"><img src={props.isDarkMode ? github_white : github_black} width="64px" alt="github link"/></a>
             <a href="https://www.linkedin.com/in/daniel-dospinescu-ba7285239/" target="_blank"><img src={props.isDarkMode ? linkedin_white : linkedin_black} width="40px" alt="linkedin link"/></a>
--- src/components/PageTitles.jsx
+++ src/components/PageTitles.jsx
@@ -1,11 +1,11 @@
 export default function PageTitles(){
     return (
         <>
-            <div className='mt-10 text-center'>
+            <div className='2xl:mt-10 lg:mt-4 text-center'>
                 <h1 className='text-2xl font-semibold font-Montserrat'>Hello, I'm Dospinescu Daniel</h1>
                 <h2 className='mt-3 font-light font-Open_Sans'>I am a programmer that uses Python and JavaScript</h2>
             </div>
-            <div className='mt-16 flex flex-col items-center justify-items-center'>
+            <div className='2xl:mt-16 lg:mt-8 flex flex-col items-center justify-items-center'>
                 <h1 className="text-3xl font-Montserrat">Personal Projects</h1>
                 <div className='mt-1 h-0.5 w-20 bg-blue-500'></div>
             </div>

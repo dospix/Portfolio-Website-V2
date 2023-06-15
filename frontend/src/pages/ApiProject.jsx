2f697ca33664a92cf4c1183f18376f2d450941b4
Daniel <dospinescu_daniel@yahoo.ro>
2023-06-15 21:32:24 +0300

    Display books on the ApiProject page, final adjustments need to be made.

--- frontend/src/pages/ApiProject.jsx
+++ frontend/src/pages/ApiProject.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import star from "../assets/images/star.png"
+import half_star from "../assets/images/half-star.png"
 
 export default function ApiProject() {
   const bookSubjectsInsideForm = {
@@ -15,36 +17,7 @@ export default function ApiProject() {
     previewFilter: "none",
     ...bookSubjectsInsideForm
   })
-
-  const [booksDisplayed, setBooksDisplayed] = useState([])
-
-  function handleFormChange(event) {
-    const {name, value, type, checked} = event.target
-    setFormData(prevFormData => ({
-        ...prevFormData,
-        [name]: type === "checkbox" ? checked : value
-      }))
-  }
-
-  function handleFormSubmit(event) {
-    event.preventDefault()
     
-    fetch("/api-project/submit", {
-    method: "POST",
-    body: JSON.stringify(formData),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-    })
-      .then(response => response.json())
-      .then(data => {
-        setBooksDisplayed(data.items.map(book => <h1>{book.volumeInfo.title}</h1>))
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      })
-  }
-
   const bookSubjectsHtmlElements = Object.keys(bookSubjectsInsideForm).map(subject => (
     <div className="flex items-center" key={subject}>
       <input
@@ -58,6 +31,94 @@ export default function ApiProject() {
       <label className="pb-2 pl-4 select-none text-3xl" htmlFor={subject}>{subject}</label>
     </div>
   ))
+
+  function handleFormChange(event) {
+    const {name, value, type, checked} = event.target
+    setFormData(prevFormData => ({
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value
+      }))
+  }
+
+  const [booksDisplayed, setBooksDisplayed] = useState([])
+
+  function convertBookObjectToHtml(book){
+    let bookData = book.volumeInfo
+
+    if(bookData.description && bookData.description.length > 300){
+        let whiteSpaceIndex = 300
+        while(whiteSpaceIndex < bookData.description.length && bookData.description[whiteSpaceIndex].trim() != "")
+            whiteSpaceIndex += 1
+        bookData.description = bookData.description.slice(0, whiteSpaceIndex) + "..."
+    }
+
+    bookData.authors = bookData.authors.slice(0, 2).join(", ")
+
+    const stars = []
+    if(bookData.averageRating){
+        // round to nearest multiple of 0.5
+        bookData.averageRating = Math.round(bookData.averageRating * 2) / 2
+
+        let needsHalfStar = bookData.averageRating % 1 == 0.5 ? true : false
+        let starCount = Math.floor(bookData.averageRating)
+        
+        while(starCount){
+            stars.push(<img key={starCount} className="mb-3 mr-2 w-6 h-6" src={star} />)
+            starCount -= 1
+        }
+        if(needsHalfStar)
+            stars.push(<img key={0.5} className="mb-3 w-4 h-6" src={half_star} />)
+    }
+
+    let previewButton = null
+    switch(book.accessInfo.viewability){
+        case "ALL_PAGES":
+            previewButton = <a className="w-28 py-1 mx-auto block rounded-2xl bg-blue-500 text-white" href={bookData.previewLink} target="_blank">View Book</a>
+            break
+        case "PARTIAL":
+            previewButton = <a className="w-32 py-1 mx-auto block rounded-2xl bg-blue-500 text-white" href={bookData.previewLink} target="_blank">View Preview</a>
+            break
+        // The remaining value shold be "NO_PAGES"
+        default:
+            previewButton = <div className="w-28 py-1 mx-auto block rounded-2xl bg-slate-400 text-white">No Preview available</div>
+    }
+    
+    return (
+        <div className="mx-48 my-10 flex" key={book.id}>
+            <img className="w-1/6" src={bookData.imageLinks.thumbnail} alt="book cover" />
+            <div className="w-full px-5">
+                <h1 className="mb-4 text-2xl">{bookData.title}</h1>
+                <h2 className="mb-4 text-xl">{bookData.subtitle}</h2>
+                <p className="font-Open_Sans">{bookData.description}</p>
+            </div>
+            <div className="w-1/5 text-center">
+                <h1 className="mb-3 text-xl">{bookData.authors}</h1>
+                <div className="flex justify-center">
+                    {stars}
+                </div>
+                <p className="mb-3">{bookData.ratingsCount ? bookData.ratingsCount + " reviews" : "no reviews"}</p>
+                {previewButton}
+            </div>
+        </div>
+    )
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    
+    fetch("/api-project/submit", {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+        "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+      .then(response => response.json())
+      .then(data => setBooksDisplayed(data.items.map(book => convertBookObjectToHtml(book))))
+      .catch(error => {
+        console.error('Error:', error);
+      })
+  }
     
   return (
   <>
@@ -69,7 +130,7 @@ export default function ApiProject() {
         <h1 className="text-3xl font-Montserrat">What type of book would you like us to recommend?</h1>
     </div>
 
-    <form onSubmit={handleFormSubmit} className="mx-auto mt-10 w-2/5 flex justify-center flex-col font-Open_Sans">
+    <form onSubmit={handleFormSubmit} className="mx-auto mt-10 mb-32 w-2/5 flex justify-center flex-col font-Open_Sans">
       <label className="p-2 text-2xl" htmlFor="titleKeywords">Title keywords:</label>
       <input 
         type="text"

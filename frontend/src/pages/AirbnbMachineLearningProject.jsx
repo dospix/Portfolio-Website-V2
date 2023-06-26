4447b3d6f52df18db705d5c952e12416ead84675
Daniel <dospinescu_daniel@yahoo.ro>
2023-06-26 14:55:03 +0300

    Cut Airbnb locations plotted from 15k to 10k.

--- frontend/src/pages/AirbnbMachineLearningProject.jsx
+++ frontend/src/pages/AirbnbMachineLearningProject.jsx
@@ -1,5 +1,9 @@
 export default function AirbnbMachineLearningProject() {
     return (
-        <h1> Airbnb machine learning project!</h1>
+        <>
+            <h1> Airbnb machine learning project!</h1>
+            <iframe src="https://www.google.com/maps/d/embed?mid=1kiDalgOCVyg7mzkolqEPFGoMYba6Di0" width="640" height="480"></iframe>
+        </>
+
     )
 }

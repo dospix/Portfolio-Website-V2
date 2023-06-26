7c93c9ef7fc337a9c911b75d68d6d0a29f567a84
Daniel <dospinescu_daniel@yahoo.ro>
2023-06-26 22:31:38 +0300

    Remove strict mode.

--- frontend/src/pages/AirbnbMachineLearningProject.jsx
+++ frontend/src/pages/AirbnbMachineLearningProject.jsx
@@ -1,8 +1,9 @@
 export default function AirbnbMachineLearningProject() {
+    
     return (
         <>
             <h1> Airbnb machine learning project!</h1>
-            <iframe src="https://www.google.com/maps/d/embed?mid=1kiDalgOCVyg7mzkolqEPFGoMYba6Di0" width="640" height="480"></iframe>
+            <iframe className="h-80 w-1/3 mx-auto" src="https://www.google.com/maps/d/embed?mid=1kiDalgOCVyg7mzkolqEPFGoMYba6Di0" />
         </>
 
     )

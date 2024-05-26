ba74e98ecf44a890ee22288266f021ea05252b4d
Daniel <102976413+dospix@users.noreply.github.com>
2024-05-26 16:31:09 +0300

    Start AWSMacronutrientProject food item form

--- frontend/src/components/PageTitles.jsx
+++ frontend/src/components/PageTitles.jsx
@@ -3,7 +3,7 @@ export default function PageTitles(){
         <>
             <div className='2xl:mt-14 lg:mt-4 sm:mt-8 mt-8 text-center'>
                 <h1 className='sm:text-2xl text-xl font-semibold font-Montserrat'>Hello, I'm Dospinescu Daniel</h1>
-                <h2 className='mt-3 lg:font-light sm:font-normal sm:text-base text-xs text-gray-500 font-Open_Sans'>I am a programmer that uses Python and JavaScript</h2>
+                <h2 className='mt-3 lg:font-light sm:font-normal sm:text-base text-xs text-gray-500 font-Open_Sans'>I am a programmer who uses Python and JavaScript</h2>
             </div>
             <div className='2xl:mt-16 lg:mt-8 sm:mt-6 mt-8 flex flex-col items-center justify-items-center'>
                 <h1 className="text-3xl font-Montserrat">Personal Projects</h1>
